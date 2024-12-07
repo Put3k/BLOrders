b37c97af6b0d5f00394081136df213a6460f04b5
Put3k <fightepucio@gmail.com>
2024-12-07 19:59:25 +0100

    changed: exe-generate - now generates per list of contractors

--- exe_generate.py
+++ exe_generate.py
@@ -2,36 +2,47 @@ import subprocess
 import os
 from pathlib import Path
 
-from constants import CONTRACTOR
-
 
 BINARY_MARK = ";."
-
-FILE_NAME = f"BLOrders-{CONTRACTOR}" if CONTRACTOR else "BLOrders"
 BASE_PATH = os.getcwd()
 MAIN_FILE = "../BLOrders.py"
 CREDENTIALS_PATH = "../credentials.json;."
 DRIVE_DESTINATION_PATH = "../drive_folders_destination.json;."
 
-
 output_path = Path(BASE_PATH) / "output"
 output_path.mkdir(parents=True, exist_ok=True)
-os.chdir("output/")
 
-# TODO: add exe generate per contractor
+os.makedirs("exe", exist_ok=True)
 
-subprocess.run(
-    [
-        "pyinstaller",
-        "--noconfirm",
-        "--onefile",
-        "--console",
-        "--name",
-        FILE_NAME,
-        "--add-data",
-        CREDENTIALS_PATH,
-        "--add-data",
-        DRIVE_DESTINATION_PATH,
-        MAIN_FILE,
-    ]
-)
+CONTRACTORS = ["FAKTORIA", "NICE"]
+
+for contractor in CONTRACTORS:
+    # Override credentials for different contractors
+    with open("constants.py", "r") as file:
+        lines = file.readlines()
+
+    with open("constants.py", "w") as file:
+        for line in lines:
+            if line.startswith("CONTRACTOR ="):
+                file.write(f"CONTRACTOR = '{contractor}'\n")
+            else:
+                file.write(line)
+
+    FILE_NAME = f"BLOrders-{contractor}" if contractor else "BLOrders"
+
+    subprocess.run(
+        [
+            "pyinstaller",
+            "--noconfirm",
+            "--onefile",
+            "--console",
+            "--name",
+            FILE_NAME,
+            "--add-data",
+            CREDENTIALS_PATH,
+            "--add-data",
+            DRIVE_DESTINATION_PATH,
+            MAIN_FILE,
+        ],
+        cwd="exe/",
+    )
